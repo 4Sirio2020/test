5d68c985116bba7a8e16efccb2947bcbfa2615c2
massimobnp <massimo.buonopane@gmail.com>
2020-05-11 18:49:49 +0200

    prima versione

---
+++ src/Esempio1.java
@@ -0,0 +1,18 @@
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Esempio1  extends Application  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 launch(args);
+	}
+	
+    public void start(Stage theStage) 
+    {
+        theStage.setTitle("Hello, World!");
+        theStage.show();
+    }
+
+}
